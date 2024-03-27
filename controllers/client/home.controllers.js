be1933c056e3eae7dccb24afa9d10c1d98b18dd2
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-03-28 02:19:07 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controllers.js
+++ controllers/client/home.controllers.js
@@ -14,8 +14,17 @@ module.exports.index = async (req, res) => {
     const newProducts = productsHelper.priceNewProducts(productsFeatured);
     // End----
 
+    // Hiển thị danh sách sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({ position: "desc" }).limit(6);
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
+    // End--------
+    
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProducts
+        productsFeatured: newProducts,
+        productsNew: newProductsNew
     });
 }
