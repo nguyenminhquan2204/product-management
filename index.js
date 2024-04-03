d83ca2e787b8b54a7d1e071c05bf8dfc449d0c44
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-04-04 03:13:49 +0700

    Làm trang 404

--- index.js
+++ index.js
@@ -61,6 +61,11 @@ app.use(express.static(`${__dirname}/public`));
 // Route
 routeAdmin(app);
 route(app);
+app.get("*", (req, res) => {
+    res.render("client/pages/errors/404", {
+        pageTitle: "404 Not Found",
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example App listening on port ${port}`);
