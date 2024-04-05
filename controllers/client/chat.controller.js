a7517d6925d201c0793b421e1f4aac0b73226ecf
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-04-05 23:05:14 +0700

    Làm tính năng chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,13 +1,42 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 
-// [GET] /chat
+// [GET] /chat/
 module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id;
     // SocketIO
-    _io.on("connection", (socket) => {
-        console.log("a user connected", socket.id);
+    _io.once("connection", (socket) => {
+        // console.log("a user connected", socket.id);
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+            // console.log(userId);
+            // console.log(content);
+
+            // Lưu vào database
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+            await chat.save();
+        });
     });
     // End SocketIO
 
+    // Lấy ra data
+    const chats = await Chat.find({
+        deleted: false
+    });
+
+    for(const chat of chats) {
+        const infouser = await User.findOne({
+            _id: chat.user_id
+        }).select("fullName");
+
+        chat.infoUser = infouser;
+    }
+    // End
+
     res.render("client/pages/chat/index", {
         pageTitle: "Chat",
+        chats: chats
     });
 };
